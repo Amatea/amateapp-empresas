681116c547c0baa80335405be2565d2682883005
Andrés Sarmiento <Sarmiento@amatea.org>
2016-09-19 13:21:14 -0500

    instalando browserify n gulp

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
+})
+
+app.listen(3000, function (){
+  console.log('Servidor en el puerto 3000')
+});
