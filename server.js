1b3bc4655e0d57069fc5ecf1214998af6cc543ad
Andrés Sarmiento <Sarmiento@amatea.org>
2016-09-23 14:29:34 -0500

    organizando el servidor

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+'use strict';
+
 var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
 
 var app = express();
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  } else if (process.env.NODE_ENV === 'production') {
+    app.use(compress());
+  }
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 app.use(express.static('public'));
 
-app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+app.set('view engine', 'html')
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
