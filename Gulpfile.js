ce5e10e93d2df375aaa3c289cc613abfb10dd73a
Andrés Sarmiento <Sarmiento@amatea.org>
2016-09-23 16:12:01 -0500

    instalacion de sass n angular-material

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
+var sass =  require('gulp-sass');
 var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+
+gulp.task('styles', function(){
+  gulp
+    .src('./src/index.scss')
+		.pipe(sass())
+		.pipe(rename('app.css'))
+		.pipe(gulp.dest('public'));
+
+})
 
 gulp.task('scripts', function(){
     browserify('./src/index.js')
@@ -22,4 +32,4 @@ gulp.task('angularjs', function(){
     .pipe(gulp.dest('public'))
 })
 
-gulp.task('default', ['scripts', 'angularjs']);
+gulp.task('default', ['styles', 'scripts', 'angularjs']);
---
+++ src/angular.js
@@ -0,0 +1,5 @@
+var app = require('angular');
+var material = require('angular-material'),
+    aria = require('angular-aria'),
+    animate = require('angular-animate');
+
